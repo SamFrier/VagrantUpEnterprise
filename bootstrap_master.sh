7ab3a9fc13e4419a57a5b0ef0cde1af86c07d904
ConnorM3 <connor.melton@qa.com>
2016-10-05 15:29:00 +0100

    Update bootstrap_master.sh
    Added installing Jira

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Hello world"
+
+sudo apt-get update
+
+#Install Git
+sudo apt-get install git
+
+#Install Jira
+cd /tmp/shared
+chmod a+x jira.bin
+./jira.bin -q -varfile response.varfile
