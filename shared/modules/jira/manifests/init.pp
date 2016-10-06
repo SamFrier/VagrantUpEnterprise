200c601b96febd5632e8124198fb1e452e1d88b2
Vagrant <vagrant@agent1.qac.local>
2016-10-06 09:42:22 -0500

    Added Jira module

---
+++ shared/modules/jira/manifests/init.pp
@@ -0,0 +1,31 @@
+class jira {
+        $FILE="/usr/share/puppet/modules/jira/files"
+        Exec {
+                path => ['/usr/bin', '/bin'],
+        }
+        file { "$FILE/jira.bin":
+                ensure => "present",
+                source => "/tmp/shared/jira.bin",
+                owner => vagrant,
+                mode => 755,
+                before => Exec['allow jira'],
+        }
+        file { "$FILE/response.varfile":
+                ensure => "present",
+                source => "/tmp/shared/response.varfile",
+                owner => vagrant,
+                before => Exec['allow jira'],
+        }
+        exec { 'allow jira':
+                cwd => "$FILE",
+                command => 'chmod a+x jira.bin',
+                before => Exec['extract jira'],
+        }
+        exec { 'extract jira':
+                cwd => "$FILE",
+		provider => shell,
+                command => './jira.bin -q -varfile response.varfile',
+        }
+}
+
+
