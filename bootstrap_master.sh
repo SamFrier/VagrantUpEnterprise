e604ce37fc40e61c25ea7681e5ecf3efc8aa7871
Vagrant <vagrant@agent1.qac.local>
2016-10-06 03:36:49 -0500

    Added the sreation of site.pp

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+SITE='/etc/puppet/manifests'
 echo "Setting up Master"
 
 sudo apt-get update
@@ -9,6 +10,17 @@ sudo apt-get install -y puppet puppetmaster
 #Edit the hosts file
 echo "Editing hosts file..."
 sed -i '1s/^/127.0.0.1	vumaster.qac.local	puppetmaster\n192.168.1.20	vumaster.qac.local	puppetmaster\n/' /etc/hosts
+
+#Add to site.pp
+echo "node 'vuagent1.qac.local', 'vuagent2.qac.local', 'vuagent.qac.local' {">>$SITE/site.pp
+echo "	include java">>$SITE/site.pp
+echo "	include maven">>$SITE/site.pp
+echo "	include git">>$SITE/site.pp
+echo "	include jira">>$SITE/site.pp
+echo "	include nexus">>$SITE/site.pp
+echo "	include bamboo">>$SITE/site.pp
+echo "	include sql">>$SITE/site.pp
+echo "}">>$SITE/site.pp
 
 #Install Git
 sudo apt-get -y install git
