639f1f0c997bb6ee9176e384f621f4379b36bb30
Vagrant <vagrant@master.qac.local>
2016-10-06 08:27:09 -0500

    edited maven and added nexus

---
+++ shared/modules/maven/maven/manifests/init.pp
@@ -0,0 +1,25 @@
+class maven {
+	
+	file { "/opt/maven.tar.gz":
+                ensure => 'present',
+		source => 'puppet:///modules/maven/maven.tar.gz',
+                before => Exec['installM'],
+        }
+
+	file { "/opt/installMaven.sh":
+		ensure => 'present',
+		source => 'puppet:///modules/maven/installMaven.sh',
+		owner => 'root',
+		group => 'root',
+		mode => '0777',
+		before => Exec['installM'],
+		notify => Exec['installM'],
+	}
+
+	exec { 'installM':
+		cwd => '/opt',
+		path => '/usr/bin',
+		provider => shell,
+		command => './installMaven.sh',
+	}
+}
