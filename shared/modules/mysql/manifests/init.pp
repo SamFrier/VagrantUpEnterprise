e0b725b1d20ab611a2e3816e8e037d87936fafd4
Sam Frier <sam.frier@qa.com>
2016-10-06 05:48:01 -0500

    Minor additions to MySQL + Git modules

--- shared/modules/mysql/manifests/init.pp
+++ shared/modules/mysql/manifests/init.pp
@@ -3,5 +3,23 @@ class mysql {
 }
 
 class mysql::install {
+    file { '/opt/mysql.tar':
+        ensure => present,
+        source => 'puppet:///modules/mysql/mysql-server-5.7.15-1ubuntu14.04_amd64.deb-bundle.tar',
+        owner => 'root',
+        group => 'root',
+    }
 
+    file { '/opt/install_mysql.sh':
+        ensure => present,
+        source => 'puppet:///modules/mysql/install_mysql.sh',
+        owner => 'root',
+        group => 'root',
+    }
+
+    exec { '/opt/install_mysql.sh':
+        provider => shell,
+        require => [File['/opt/mysql.tar'], File['/opt/install_mysql.sh']],
+        # NEED ONLYIF HERE!!!!
+    }
 }
