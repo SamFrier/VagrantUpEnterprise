6d48340d2c57ac92a0ce99b68299398a1dd237b1
Vagrant <vagrant@agent1.qac.local>
2016-10-06 10:29:41 -0500

    Added file edits to agent bootstrap

--- bootstrap_agent.sh
+++ bootstrap_agent.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 echo "Hello world"
+ADDRESS=$(facter ipaddress_eth1)
+NAME=$(facter fqdn)
 
 sudo apt-get update
 
+#Install puppet
 sudo apt-get install -y puppet
+#Edit the hosts file
+sed -i "1s/^/$ADDRESS	$NAME	puppet\n/" /etc/hosts
+sed -i "1s/^/127.0.0.1	$NAME	puppet\n/" /etc/hosts
+sed -i "1s/^/192.168.1.23	vumaster.qac.local	puppetmaster\n/" /etc/hosts
+
+#Edit the conf file
+sed -i 's/[main]/[main\nserver=vumaster.qac.local/g' /etc/puppet/puppet.conf
 
 # install git
 sudo apt-get install -y git
