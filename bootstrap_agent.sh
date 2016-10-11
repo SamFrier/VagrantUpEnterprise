b85b45c0726a91db082037d1a232677db8afa162
danielperryQA <daniel.perry@qa.com>
2016-10-11 16:11:31 +0100

    Update bootstrap_agent.sh

--- bootstrap_agent.sh
+++ bootstrap_agent.sh
@@ -4,25 +4,28 @@ echo "Setting up an agent"
 
 sudo apt-get update
 sudo apt-get install -y openssh-server openssh-client
+sudo apt-get install -y curl
 sudo ufw disable
+
+sudo chown -R vagrant /home/vagrant/.ssh
+sudo chmod 0700 /home/vagrant/.ssh
+sudo chmod 0600 /home/vagrant/.ssh/authorized_keys
 
 #Install puppet
 sudo apt-get install -y puppet
+
 echo "Editing hosts file..."
 #Edit the hosts file
-ADDRESS=$(facter ipaddress_eth1)
-NAME=$(facter fqdn)
-sed -i "1s/^/$ADDRESS	$NAME	puppet\n/" /etc/hosts
-sed -i "1s/^/127.0.0.1	$NAME	puppet\n/" /etc/hosts
-sed -i "1s/^/192.168.1.20	vumaster.qac.local	puppetmaster\n/" /etc/hosts
+sed -i "1s/^/192.168.1.108	entmaster.qac.local	entmaster\n/" /etc/hosts
 
 #Edit the conf file
 echo "Editing the puppet.conf file..."
-sed -i 's/ain]/ain]\nserver=vumaster.qac.local/g' /etc/puppet/puppet.conf
+sed -i 's/ain]/ain]\nserver=entmaster.qac.local/g' /etc/puppet/puppet.conf
 
 #Connect to the master
 echo "Managing communications with master client..."
-sudo puppet agent --test --server=vumaster.qac.local
+sudo curl -k https://entmaster.qac.local:8140/packages/current/install.bash | sudo bash
+sudo puppet agent --test --server=entmaster.qac.local
 sudo service puppet stop
 sudo service puppet start
 sudo puppet agent --enable
