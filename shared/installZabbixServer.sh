6b694710796eb465e6100d2ed7d96dd83bd36629
danielperryQA <daniel.perry@qa.com>
2016-10-13 16:56:06 +0100

    New zabbix
    with gui

--- shared/installZabbixServer.sh
+++ shared/installZabbixServer.sh
@@ -4,22 +4,22 @@ echo "Installing Zabbix Server"
 
 # unzip zabbix server files
 cd /opt/
-sudo tar zxvf zabbix-3.2.1.tar.gz
+sudo tar zxf zabbix-3.2.1.tar.gz
 
 # create user account
 sudo groupadd zabbix
 sudo useradd -g zabbix zabbix
 
 # install mysql on master
-sudo puppet apply /etc/puppetlabs/code/environments/production/modules/mysql/tests/init.pp
+sudo puppet apply /etc/puppet/modules/mysql/tests/init.pp
 
 # create zabbix database
 sudo cp /tmp/shared/setupDatabase.sql /opt/
 sudo mysql -u root < setupDatabase.sql
 cd zabbix-3.2.1/database/mysql/
-sudo mysql -u zabbix zabbix < schema.sql
-sudo mysql -u zabbix zabbix < images.sql
-sudo mysql -u zabbix zabbix < data.sql
+sudo mysql -u zabbix -pvagrantup zabbix < schema.sql
+sudo mysql -u zabbix -pvagrantup zabbix < images.sql
+sudo mysql -u zabbix -pvagrantup zabbix < data.sql
 
 # configure sources
 cd ../..
@@ -29,11 +29,29 @@ sudo ./configure --enable-server --with-mysql
 sudo make install
 
 # edit config files
-
+sudo sed -i 's/# DBPassword=/# DBPassword=\n\nDBPassword=vagrantup/g' /usr/local/etc/zabbix_server.conf
 
 # start server daemon
-zabbix_server
+sudo zabbix_server
 
-# might need to do some stuff with the PHP frontend here?
+# setup apache web server and php
+sudo apt-get install -y apache2 apache2-doc apache2-utils
+sudo sed -i 's/KeepAlive On/KeepAlive Off/g' /etc/apache2/apache2.conf
+sudo apt-get install -y php5-mysql php5-gd
+sudo sed -i 's/max_size = 8/max_size = 16/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/on_time = 30/on_time = 300/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/max_input_time = 60/max_input_time = 300/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/;date.timezone =/date.timezone = UTC/g' /etc/php5/apache2/php.ini
 
-echo "Zabbix Server installed."
+sudo a2dissite 000-default.conf
+sudo service apache2 reload
+sudo apt-get install -y php5
+
+# setup web-based frontend
+sudo mkdir /var/www/zabbix
+cd frontends/php
+sudo cp -a . /var/www/zabbix
+
+sudo service apache2 restart
+
+echo "Zabbix Server installed. Visit http://<server_ip>/zabbix to complete setup."
